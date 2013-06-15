fd80681c3bfdde7fc41ad8ca1f940154fbe68cf4
Grayson Wright <wright.grayson@gmail.com>
2013-06-15 12:59:20 +0800

    Use absolute paths for symbolic links

--- install.rb
+++ install.rb
@@ -4,6 +4,6 @@ files = `ls source`.split
 
 files.each do |file|
   print "Linking #{file}...\t"
-  `ln -s .dotfiles/source/#{file} ~/.#{file}`
+  `ln -s \`pwd\`/source/#{file} ~/.#{file}`
   puts "Done"
 end
