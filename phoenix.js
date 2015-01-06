05d3f0d106ada6efa2bcbc17c290b765e2bd0f5f
Grayson Wright <wright.grayson@gmail.com>
2015-01-05 16:55:36 -0800

    Set up moving from one screen to another

--- phoenix.js
+++ phoenix.js
@@ -87,11 +87,48 @@ Window.prototype.toFullScreen = function() {
   return this.toGrid( 0, 0, 1, 1 );
 };
 
+function moveToPreviousScreen() {
+  var window = Window.focusedWindow();
+  moveToScreen(window, window.screen().previousScreen());
+};
+
+function moveToNextScreen() {
+  var window = Window.focusedWindow();
+  moveToScreen(window, window.screen().nextScreen());
+};
+
+function moveToScreen(window, screen) {
+  if (!window) return;
+  if (!screen) return;
+
+  var frame = window.frame();
+  var oldScreenRect = window.screen().frameWithoutDockOrMenu();
+  var newScreenRect = screen.frameWithoutDockOrMenu();
+  var xRatio = newScreenRect.width / oldScreenRect.width;
+  var yRatio = newScreenRect.height / oldScreenRect.height;
+
+  var mid_pos_x = frame.x + Math.round(0.5 * frame.width);
+  var mid_pos_y = frame.y + Math.round(0.5 * frame.height);
+
+  window.setFrame({
+    x: (mid_pos_x - oldScreenRect.x) * xRatio + newScreenRect.x - 0.5 * frame.width,
+    y: (mid_pos_y - oldScreenRect.y) * yRatio + newScreenRect.y - 0.5 * frame.height,
+    width: frame.width,
+    height: frame.height
+  });
+
+  disableKeys();
+};
+
 bind('f', mNone, function() { Window.focusedWindow().toFullScreen(); });
 bind('h', mNone, function() { toGrid(0, 0, 0.5, 1); });
 bind('l', mNone, function() { toGrid(0.5, 0, 0.5, 1); });
 bind('j', mNone, function() { toGrid(0, 0.5, 1, 0.5); });
 bind('k', mNone, function() { toGrid(0, 0, 1, 0.5); });
+
+bind('h', mShift, moveToPreviousScreen);
+bind('l', mShift, moveToNextScreen);
+bind('s', mNone, moveToNextScreen);
 
 // Initially disable all hotkeys
 disableKeys();
