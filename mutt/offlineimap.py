3dc1c53af181282b4c138af8b1dd9d1d2efb01e7
Grayson Wright <wright.grayson@gmail.com>
2016-01-02 17:22:14 -0500

    Log into lastpass when opening Mutt
    Problem:
    
    The current setup for offlineimap makes users sign in to lastpass
    every time that we try to sync email with the server.
    
    This gets a bit annoying, and is inefficient.
    
    Solution:
    
    - Check to see if we're already logged in to lastpass
      before requiring the user to log in again.
    - Prompt the user to log into lastpass
      any time mutt is opened through the `mail` alias.

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -5,7 +5,7 @@ def get_lpass_pass(site=None):
         'lpass': '/usr/local/bin/lpass',
         'site': site,
     }
-    command = "/usr/local/bin/lpass login wright.grayson@gmail.com && /usr/local/bin/lpass show \"Email/google.com\""
+    command = "(/usr/local/bin/lpass ls Email/google.com || /usr/local/bin/lpass login wright.grayson@gmail.com) && /usr/local/bin/lpass show Email/google.com"
     output = subprocess.check_output(command, shell=True)
     outtext = [l for l in output.splitlines()
                if l.startswith('Password: ')][0]
